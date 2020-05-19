ef5fb5925167c0d5011c83401ba452e1e59cbf69
urbanswelt <urbanswelt@gmail.com>
2020-05-19 21:14:46 +0200

    Update xsil_fileTable.lua

--- xsil_lua_files/xsil_fileTable.lua
+++ xsil_lua_files/xsil_fileTable.lua
@@ -1,3 +1,3 @@
 xsil_update = {}
-xsil_update[1] = { url = xsil_Github.domain .. xsil_Github.activbranch .. "/xsil_lua_files/github_master.php", data = "0", size = "0", localsize = "0", status = "0", filename = "github_master.php" }
-xsil_update[2] = { url = xsil_Github.domain .. xsil_Github.activbranch .. "/xsil_lua_files/github_development.php", data = "0", size = "0", localsize = "0", status = "0", filename = "github_development.php" }
+xsil_update[1] = { url = xsil_Github.domain .. xsil_Github.activbranch .. "/xsil_lua/xsil_lua_files/github_master.php", data = "0", size = "0", localsize = "0", status = "0", filename = "github_master.php" }
+xsil_update[2] = { url = xsil_Github.domain .. xsil_Github.activbranch .. "/xsil_lua/xsil_lua_files/github_development.php", data = "0", size = "0", localsize = "0", status = "0", filename = "github_development.php" }
