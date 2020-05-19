751290570a93004b03b05fd9a50f28692e735d85
urbanswelt <urbanswelt@gmail.com>
2020-05-19 11:07:51 +0200

    Update and rename testtable.lua to xsil_fileTable.lua

--- xsil_lua_files/testtable.lua
+++
@@ -1,7 +0,0 @@
-xac_update = {}
-xac_update.updater = { url = "http://xac.urbanswelt.de/develop/XAutoCopilot/scripts/xac_lua/logic/xac_window_XAutoCopilotUpdater.lua", data = "", size = "", localsize = "", status = "0", filename = "" }
-xac_update.update = { url = xsil_Github.domain.name .. xsil_Github.isActive.branch .. "/xac_lua/logic/xac_window_XAutoCopilotUpdater.lua", data = "", size = "", localsize = "", status = "0", filename = "" }
-
-
-
- 
---
+++ xsil_lua_files/xsil_fileTable.lua
@@ -0,0 +1,7 @@
+xsil_update = {}
+xsil_update.updater = { url = xsil_Github.domain.name .. xsil_Github.isActive.branch .. "/xsil_autoupdater.lua", data = "", size = "", localsize = "", status = "0", filename = "" }
+xsil_update.update = { url = xsil_Github.domain.name .. xsil_Github.isActive.branch .. "/xac_lua/logic/xac_window_XAutoCopilotUpdater.lua", data = "", size = "", localsize = "", status = "0", filename = "" }
+
+
+
+ 
