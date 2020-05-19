4e302f2e2d94ae66bd6953130e3fbe7dd94dc571
urbanswelt <urbanswelt@gmail.com>
2020-05-19 10:50:27 +0200

    Update testtable.lua

--- xsil_lua_files/testtable.lua
+++ xsil_lua_files/testtable.lua
@@ -1,2 +1,7 @@
 xac_update = {}
 xac_update.updater = { url = "http://xac.urbanswelt.de/develop/XAutoCopilot/scripts/xac_lua/logic/xac_window_XAutoCopilotUpdater.lua", data = "", size = "", localsize = "", status = "0", filename = "" }
+xac_update.update = { url = xsil_Github.domain.name .. xsil_Github.isActive.branch .."/xac_lua/logic/xac_window_XAutoCopilotUpdater.lua", data = "", size = "", localsize = "", status = "0", filename = "" }
+
+
+
+ 
